acacfde5aeb00e0df5d487a77eb5377017fe960f
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2021-05-21 14:56:16 +0530

    commit 5

--- src/main/scala/Spark/lead_lag.scala
+++ src/main/scala/Spark/lead_lag.scala
@@ -3,6 +3,7 @@ package Spark
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
+
 object lead_lag extends App{
 
   val spark= SparkSession.builder().appName("lead and lag").master("local").getOrCreate()
@@ -15,15 +16,25 @@ import spark.implicits._
   ("C","1","10","DOWN")).toDF("server","start","end","status")
   //a.printSchema()
   //a.show()
+  //lead function
   val window= Window.partitionBy("server").orderBy("server")
   val s= lead(col("start"),1,0).over(window)
   val b=a.withColumn("desc",s)
   //b.show()
   val c= b.where("desc !=0")
-    c.show()
+   // c.show()
   val d= c.select (col("server"),col("status"),col("end"),col("status")
     ,(col("desc")-col("end")).as("new_data")).where ("new_data != 0.0")
-  d.show()
+  //d.show()
 
+  // lag function
 
+ // a.show()
+
+  val g= Window.partitionBy("server").orderBy("server")
+  val h= lag(col("end"),1,0).over(g)
+  val i= a.withColumn("lag",h).where("lag !=0")
+  val j= i.select (col("server"),col("status"),col("end"),col("status")
+    ,(col("start")-col("lag")).as("new_data")).where ("new_data != 0.0")
+  j.show()
 }
